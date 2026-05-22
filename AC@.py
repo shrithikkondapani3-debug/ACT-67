34abacc84bec8fb54789e6555b53865b00245ec5
shrithikkondapani3-debug <shrithikkondapani3@gmail.com>
2026-05-22 16:18:59 +0530

    Add files via upload

---
+++ AC@.py
@@ -0,0 +1,17 @@
+# Printing the title
+print("My Daily Routine")
+
+# Step 1: Waking up
+print("I wake up at 8 AM.")
+
+# Step 2: Going to school
+print("I go to school at 9 AM.")
+
+# Step 3: Playing after school
+print("I play with my friends in the evening.")
+
+# Step 4: Studying
+print("I study for 0 hours at night.")
+
+# Calculating total study time in a week
+print("Total study hours in a week:", 0 * 1)
